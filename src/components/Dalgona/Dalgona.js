fe5befd0e54ba8266ee5af9bd9a9dff9f33c896f
Annie Lee <annie80113@hotmail.com>
2021-10-14 16:39:57 -0400

    created dalgona component

---
+++ src/components/Dalgona/Dalgona.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+  
+function Dalgona() {
+
+
+
+  return (
+    <div className="gameContainer dalgona">
+  
+{/* 
+      <div className='playWindow-left'>
+        
+      </div> */}
+
+    </div>
+  );
+}
+  
+  export default Dalgona;
+  
+  // create one green piece 
+  // create red/green light sign
+  // when the green piece is clicked during green light, move toward end line
+  // when the green piece is clicked during red light, the green piece turn red
+  // if the green piece reach end line, show the next button
+  // when the timer goes 0, show the next button
+
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,17 +1,54 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import GameRLGL from '../GameRLGL/GameRLGL'
-  
-  function Game() {
+import Caption from '../Caption/Caption';
+import Timer from '../Timer/Timer';
+import RLGL from '../RLGL/RLGL'
+import Dalgona from '../Dalgona/Dalgona'
 
-    return (
-      <div className="gameContainer">
-        <Route path='/game/rlgl' render={() => 
-          <GameRLGL />
-        } />
-      </div>
-    );
+
+function Game() {
+  let titleDalgona = "Dalgona Candy"
+  let rulesDalgona = "Click on all the dots to make the shape out"
+  let titleRLGL = "Red Light, Green Light"
+  let rulesRLGL = "Click on the green pieces to make them move, only move when it's green light"
+  let minute = 0
+  let second = 0
+  let start = true
+
+  function handleStart() {
+    console.log(start)
+    start = false
+    console.log(start)
   }
-  
-  export default Game;
-  
+
+  return (
+    <div className="gameContainer">
+      <Route path='/game/rlgl' render={() => 
+        <div className='screen'>
+          <Timer min={minute} sec={second} />
+          <Caption title={titleRLGL} rules={rulesRLGL} />
+          <div className='startBtn'>
+            <button onClick={handleStart}>Start</button>
+          </div>
+        </div>
+      } />
+      <Route path='/game/dalgona' render={() => 
+        <div className='screen'>
+          <Timer min={minute} sec={second} />
+          <Caption title={titleDalgona} rules={rulesDalgona} />
+          <div className='startBtn'>
+            <button onClick={handleStart}>Start</button>
+          </div>
+        </div>
+      } />
+      <main>
+        {/* Setting route to each pages */}
+        <Route path='/game/rlgl' component={RLGL} />
+        <Route path='/game/dalgona' component={Dalgona} />
+      </main>
+
+    </div>
+  );
+}
+
+export default Game;
