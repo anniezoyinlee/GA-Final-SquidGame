0891a3c87d534a385d81de0b36a67ce02660332a
Annie Lee <annie80113@hotmail.com>
2021-10-14 10:59:46 -0400

    updated timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,21 +1,42 @@
 import React, { useState, useEffect } from 'react'
 
-function Timer() {
-  const [seconds, setSeconds] = useState(100);
+// reference: https://www.codegrepper.com/code-examples/javascript/react+countdown+timer+minutes+seconds
+const Timer = () => {
+  const initialMinute = 0
+  const initialSeconds = 5
+  const [ minutes, setMinutes ] = useState(initialMinute);
+  const [ seconds, setSeconds ] =  useState(initialSeconds);
 
-  useEffect(() => {
-    if (seconds > 0) {
-      setTimeout(() => setSeconds(seconds - 1), 1000);
-    } else {
-      setSeconds('----------');
-    }
+  useEffect(()=>{
+  let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      } 
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval)
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      } 
+    }, 1000)
+    return ()=> {
+      clearInterval(myInterval);
+    };
   });
 
   return (
-    <div className="timer">
-      {seconds}
-    </div>
-  );
+      <div className='timer'>
+      { minutes === 0 && seconds === 0
+          ? <h1>00:00</h1> 
+          : <h1>{minutes < 10 ?  `0${minutes}` : minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
+      }
+      </div>
+  )
 }
 
 export default Timer;
+
+
+// numbers have difference spacing when counting down, need to find a way to fix it
