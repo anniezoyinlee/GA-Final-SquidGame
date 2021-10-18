7951a137a17a5dacff2677259d9f0f5707d5b91a
Annie Lee <annie80113@hotmail.com>
2021-10-17 20:06:14 -0400

    updated the glass stepping stone page

--- src/components/Game/TGSS/TGSS.js
+++ src/components/Game/TGSS/TGSS.js
@@ -7,6 +7,33 @@ function TGSS() {
   return (
     <div className="tgss">
 
+      <div className='players'>
+        <div className='player'></div>
+        <div className='player'></div>
+      </div>
+
+      <div className='bridges'>
+        <div className='bridge up'>
+          <div className='glass up'></div>
+          <div className='glass up'></div>
+          <div className='glass up'></div>
+          <div className='glass up'></div>
+          <div className='glass up'></div>
+          <div className='glass up'></div>
+          <div className='glass up'></div>
+          <div className='glass up'></div>
+        </div>
+        <div className='bridge down'>
+        <div className='glass up'></div>
+          <div className='glass down'></div>
+          <div className='glass down'></div>
+          <div className='glass down'></div>
+          <div className='glass down'></div>
+          <div className='glass down'></div>
+          <div className='glass down'></div>
+          <div className='glass down'></div>
+        </div>
+      </div>
     </div>
   );
 }
