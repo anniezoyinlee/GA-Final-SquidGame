bec8bee4332c5a99a308511f01eba81553456fde
Annie Lee <annie80113@hotmail.com>
2021-10-18 17:58:24 -0400

    tried something

--- src/components/Game/RLGL/RLGL.js
+++ src/components/Game/RLGL/RLGL.js
@@ -3,27 +3,35 @@ import './RLGL.css';
   
 function RLGL() {
   // const [life, setLife] = useState(456);
-  const lights = ['Red Light', 'Green Light']
+
   let index = 0
+
+  const [gameOver, setGameOver] = useState(false);
 
   const [offsetRight001, setOffsetRight001] = useState(0);
   const [offsetRight002, setOffsetRight002] = useState(0);
   const [offsetRight003, setOffsetRight003] = useState(0);
   const [offsetRight004, setOffsetRight004] = useState(0);
 
+
+  const lights = ['Red Light', 'Green Light']
   function changeLight() {
-    index = index + 1;
-    if (index == lights.length)
-        index = 0;
+   
 
-    let status = document.getElementById('lightSign');
-    status.innerText = lights[index];
+    if (!gameOver) {
+      index = index + 1;
+      if (index === lights.length)
+          index = 0;
+  
+      let status = document.getElementById('lightSign');
+      status.innerText = lights[index];
+      if (status.innerText === 'Red Light') {
+        status.classList = 'redlight'
+      } else if (status.innerText === 'Green Light') {
+        status.classList = 'greenlight'
+      }
+    } 
 
-    if (status.innerText === 'Red Light') {
-      status.classList = 'redlight'
-    } else if (status.innerText === 'Green Light') {
-      status.classList = 'greenlight'
-    }
   };
 
   //Setting interval
@@ -45,11 +53,14 @@ function RLGL() {
   }
 
   function completeRLGL() {
-    //Clearing interval
-    clearInterval(lightInterval);
+    console.log('test')
+
     let status = document.getElementById('lightSign');
     // remove red light/green light sign
     status.innerText = '';
+    setGameOver(true);
+    //Clearing interval
+    clearInterval(lightInterval);
   }
   
   // reference: https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
@@ -66,9 +77,12 @@ function RLGL() {
         <div className='ground'>
           <div className='player' onClick={(e) => {
             movePlayerRight001()
-            console.log(getOffset(e.currentTarget).left)
+            // completeRLGL()
+            // console.log(getOffset(e.currentTarget).left)
             if (getOffset(e.currentTarget).left > 900) {
+              console.log(getOffset(e.currentTarget).left)
               completeRLGL() 
+              e.currentTarget.style.pointerEvents = 'none';
             }
           }} style={{
           transform: `translateX(${offsetRight001}px)`
