4cff0fecd02b3e9e5013f0ae28ed49bef44c4dd5
Annie Lee <annie80113@hotmail.com>
2021-10-19 16:25:42 -0400

    updated pesudo code for rlgl page

--- src/components/Game/RLGL/RLGL.js
+++ src/components/Game/RLGL/RLGL.js
@@ -111,9 +111,9 @@ function RLGL() {
   
 export default RLGL;
   
-// create one green piece 
-// create red/green light sign
+// fix setInterval issues
 // when the green piece is clicked during green light, move toward end line
-// when the green piece is clicked during red light, the green piece turn red
-// if the green piece reach end line, show the next button
-// when the timer goes 0, show the next button
+// when the green piece is clicked during red light, the green piece turns not clickable
+// if all the clickable green pieces reach the end line, game is over
+// when the timer goes 0, no green pieces are clickable
+// when the game is over, do alive player count and show the next button
