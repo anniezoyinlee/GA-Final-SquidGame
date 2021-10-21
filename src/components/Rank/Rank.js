446c4fdb18a68ad3713c2587ae84b566a418d04e
Annie Lee <annie80113@hotmail.com>
2021-10-20 20:32:03 -0400

    connected to firebase

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,53 +1,72 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useHistory } from 'react-router-dom'
 import './Rank.css';
+import firebase from '../../firebase'
+import AddName from "../enter-rank-name";
+
+// reference: https://www.youtube.com/watch?v=rSgbYCdc4G0
+function useLives() {
+  const [lives, setLives] = useState([])
+
+  useEffect(() => {
+    const unsubscribe = firebase
+      .firestore()
+      .collection('lives')
+      .onSnapshot((snapshot) => {
+        const newLives = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }))
+
+        setLives(newLives)
+      })
+    
+    return () => unsubscribe()
+  }, [])
+
+  return lives
+}
 
 function Rank() {
   const history = useHistory();
+  const lives = useLives();
+  const [name, setName] = useState('')
+  const [score, setScore] = useState('')
+
+  function onSubmit(e) {
+    e.preventDefault()
+
+    firebase
+      .firestore()
+      .collection(lives)
+      .add({
+        name,
+        score: parseInt(score)
+      })
+      .then(() => {
+        setName('')
+        setScore('')
+      })
+  }
 
   return (
     <div className="rank">
       <button onClick={() => history.goBack()}>Go Back</button>
 
       <div className='leaderboard'>
-
+        <h1>Rank</h1>
         <ol>
-          <li>
+          {lives.map((life) => 
+          <li key={life.id}>
             <div className='name-entry'>
-              Annie
-              <code>456</code>
+              {life.name}
+              <code> {life.score}</code>
             </div>
           </li>
-          <li>
-            <div className='name-entry'>
-              Annie
-              <code>456</code>
-            </div>
-          </li>
-          <li>
-            <div className='name-entry'>
-              Annie
-              <code>456</code>
-            </div>
-          </li>
-          <li>
-            <div className='name-entry'>
-              Annie
-              <code>456</code>
-            </div>
-          </li>
+          )}
         </ol>
 
-        <form>
-          <div>
-            <h4>Enter Your Name</h4>
-            <label>Your Name</label>
-            <input type='text'></input>
-            <label>Life</label>
-            <input type='number'></input>
-          </div>
-          <button>Add Rank</button>
-        </form>
+        <AddName />
       </div>
     </div>
   );
--- src/components/enter-rank-name.jsx
+++ src/components/enter-rank-name.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useState}  from 'react'
+import firebase from '../firebase'
 
 const AddName = () => {
+  const [name, setName] = useState('')
+  const [score, setScore] = useState('')
+
+  function onSubmit(e) {
+    e.preventDefault()
+
+    firebase
+      .firestore()
+      .collection('lives')
+      .add({
+        name,
+        score: parseInt(score)
+      })
+      .then(() => {
+        setName('')
+        setScore('')
+      })
+  }
   
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div>
         <h4>Enter Your Name</h4>
         <label>Your Name</label>
-        <input type='text'></input>
-        <label>Life</label>
-        <input type='number'></input>
+        <input type='text' value={name} onChange={e => setName(e.currentTarget.value)}></input>
+        <label>Score</label>
+        <input type='number'value={score} onChange={e => setScore(e.currentTarget.value)}></input>
       </div>
       <button>Add Rank</button>
     </form>
